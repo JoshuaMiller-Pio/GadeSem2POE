5af00b03525dbfc7c3344e4c8fe6e2bbcab0f8a1
Joshua Miller-Pio <joshuapio585@gmail.com>
2024-08-31 18:14:05 +0200

    upload nav mesh and SO

---
+++ Assets/Scripts/Terrain/Tile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public TileScriptable tileScriptable;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
